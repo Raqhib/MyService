ab6ac139877b98809834a4b5c74a0d5265d0578c
Raqhib <raqhib.ataillah@gmail.com>
2024-11-21 00:21:15 +0800

    Latihan background Service

---
+++ app/src/main/java/id/co/mondo/myservice/MainActivity.kt
@@ -0,0 +1,29 @@
+package id.co.mondo.myservice
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.co.mondo.myservice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val serviceIntent = Intent(this, MyBackgroundService::class.java)
+
+        binding.btnStartBackgroundService.setOnClickListener {
+            startService(serviceIntent)
+        }
+
+        binding.btnStopBackgroundService.setOnClickListener {
+            stopService(serviceIntent)
+        }
+
+
+    }
+}
